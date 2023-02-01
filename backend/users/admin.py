9e2f34df7c69a7655f26f1e3f209e8227702c5b3
Дмитрий Ушаков <udv8@ya.ru>
2023-02-01 23:43:03 +0300

    v0.1.1 Create User model

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email',
+                    'username',
+                    'first_name',
+                    'last_name',
+                    'is_superuser',
+                    'is_active',
+                    )
+    search_fields = ('email', 'username', 'first_name', 'last_name',)
+    ordering = ('id', 'email', 'username',)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    """Модель пользователя."""
+    email = models.EmailField('Email', max_length=150, unique=True)
+    username = models.CharField('Логин', max_length=150)
+    first_name = models.CharField('Имя', max_length=150, blank=True)
+    last_name = models.CharField('Фамилия', max_length=150, blank=True)
+    password = models.CharField('Пароль', max_length=128)
+    is_superuser = models.BooleanField('Администратор', default=False)
+    is_active = models.BooleanField('Активный', default=True)
+
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['email', 'username']
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        ordering = ('-pk',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
