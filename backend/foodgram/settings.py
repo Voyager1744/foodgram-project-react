e12ed8e2291c8e7bcde0a7abceefdcf17bc45283
Дмитрий Ушаков <udv8@ya.ru>
2023-02-11 16:49:54 +0300

    v0.2.7 -- Add Paginator

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 6
+}
