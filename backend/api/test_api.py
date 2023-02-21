47d275353ac98225a66b1f2056142068393e61ee
Дмитрий Ушаков <udv8@ya.ru>
2023-02-21 23:42:49 +0300

    v0.6.1 -- Add smoke test

--- backend/api/test_api.py
+++ backend/api/test_api.py
@@ -1,9 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
+
+User = get_user_model()
 
 
 class SmokeTest(TestCase):
     def setUp(self) -> None:
         self.guest_client = Client()
+        self.user = User.objects.create_user(username='HasNoName',
+                                             email='test@test.ru',
+                                             password='p@ssword')
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.user)
 
     def test_recipes(self):
         response = self.guest_client.get('/api/recipes/')
@@ -11,4 +19,8 @@ class SmokeTest(TestCase):
 
     def test_users(self):
         response = self.guest_client.get('/api/users/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_me(self):
+        response = self.authorized_client.get('/api/users/')
         self.assertEqual(response.status_code, 200)
